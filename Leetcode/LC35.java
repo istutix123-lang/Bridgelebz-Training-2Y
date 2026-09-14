d7fc4e47ef8027886bd8e4a77a2b22c6baad73dc
istuti123-lang <istutix123@gmail.com>
2026-09-14 12:51:31 +0530

    Search Insert Position

---
+++ Leetcode/LC35.java
@@ -0,0 +1,13 @@
+class LC35 {
+    public static void main(String[]args) {
+      int []  nums = {1,3,5,6};
+      int target = 5;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target) {
+                System.out.println(i);;
+            }
+        }
+      //  System.out.println(nums.length);;
+    }
+}
+
